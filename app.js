f5485011cb1ad3f9fffc8784b710fae774852f28
Etzio <carldrewx@gmail.com>
2025-04-22 00:04:44 +0800

    New Repo

---
+++ app.js
@@ -0,0 +1,19 @@
+function gotoLinkOne(link){
+    location.href = 'https://car98k.itch.io/future-in-ruins';
+}
+
+function gotoLinkTwo(link){
+    location.href = 'https://car98k.itch.io/demon-hunter';
+}
+
+function gotoLinkThree(link){
+    location.href = 'https://car98k.itch.io/how-i-apply';
+}
+
+function gotoLinkFour(link){
+    location.href = 'https://car98k.itch.io/raining-colors';
+}
+
+function gotoLinkFive(link){
+    location.href = 'https://car98k.itch.io/zombie-hunter-endless';
+}
